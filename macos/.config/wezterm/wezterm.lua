0bc3e9b24e79bbc4f34597089e53bdeed96b705f
Tomislav Nikic <tomislav@nikic.org>
2024-04-23 14:14:49 +0200

    Modifying wezterm
    I removed the border as well as the tab section and increased the
    initial size to full screen.

--- macos/.config/wezterm/wezterm.lua
+++ macos/.config/wezterm/wezterm.lua
@@ -1,6 +1,11 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
+config.enable_tab_bar = false
+config.window_decorations = "RESIZE"
+config.initial_rows = 999
+config.initial_cols = 999
+
 config.color_scheme = 'Catppuccin Macchiato'
 config.font = wezterm.font 'JetBrainsMono Nerd Font'
 config.font_size = 20
